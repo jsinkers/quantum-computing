9c7736319212766c94acd9891f746d08c8d6fe51
James Sinclair <sinclair.j@gmail.com>
2020-12-06 22:25:46 +1100

    added more complex number functionality

--- ComplexNumbers.hs
+++ ComplexNumbers.hs
@@ -25,7 +25,11 @@ module ComplexNumbers
     cvAdd,
     cvScalarMult,
     cvInverse,
+    cvInnerProd,
     cvDot,
+    cvConjugate,
+    cvNorm,
+    cvDist,
     cmIdentity,
     cmAdd,
     cmScalarMult,
@@ -35,11 +39,18 @@ module ComplexNumbers
     cmAdjoint,
     cmConjugate,
     cmMult,
+    cmInnerProd,
+    cmTrace,
+    cmNorm,
     printComplexMatrix,
     showComplexMatrix
    ) where 
 
 import Data.List (transpose, intercalate)
+
+-- todo: introduce a type class for inner space product etc for polymorphic
+-- functions, instead of functions particular to vectors and matrices
+
 -- data types
 -- ============================================================================
 type Re = Double
@@ -50,6 +61,7 @@ type Theta = Double
 
 data ComplexNumber = Cartesian Re Im | Euler Rad Theta
 
+
 type ComplexVector = [ComplexNumber]
 
 -- m x n complex matrix: contains m lists of n elements
@@ -58,6 +70,10 @@ type ComplexMatrix = [[ComplexNumber]]
 instance Show ComplexNumber where
     show (Cartesian r i) = show r ++ " + " ++ show i ++ "i"
     show (Euler r t) = show r ++ "e^{i" ++ show t ++ "}"
+instance Eq ComplexNumber where
+    a == b = complexRe a' == complexRe b' && complexIm a' == complexIm b'
+        where a' = toCartesian a
+              b' = toCartesian b
 
 -- complex number algebra
 -- ============================================================================
@@ -152,9 +168,35 @@ cvScalarMult s = map (complexMult s)
 cvInverse :: ComplexVector -> ComplexVector
 cvInverse = map (complexSub (Euler 0 0)) 
 
--- dot product of two complex vectors
+-- helper for matrix calculations
+cvProduct :: ComplexVector -> ComplexVector -> ComplexNumber
+cvProduct cv1 cv2 =  foldl complexAdd complexZero $ zipWith complexMult cv1 cv2
+
+-- inner product/dot product
+cvInnerProd :: ComplexVector -> ComplexVector -> ComplexNumber
+cvInnerProd v1 v2 = if length v1 == length v2
+                       then cvProduct (cvConjugate v1) v2
+                       else error "ComplexVector size mismatch"
+
+-- dot product: alias for inner product
 cvDot :: ComplexVector -> ComplexVector -> ComplexNumber
-cvDot cv1 cv2 =  foldl complexAdd complexZero $ zipWith complexMult cv1 cv2
+cvDot = cvInnerProd
+
+-- take conjugate of a complex vector
+cvConjugate :: ComplexVector -> ComplexVector
+cvConjugate = map complexConjugate 
+
+-- determine norm of a complex vector
+cvNorm :: ComplexVector -> Double
+cvNorm v = sqrt $ complexRe $ cvInnerProd v v 
+
+-- determine distance between two complex vectors
+cvDist :: ComplexVector -> ComplexVector -> Double
+cvDist v1 v2 = cvNorm $ cvAdd v1 $ cvInverse v2
+
+-- determine if two complex vectors are orthogonal
+cvOrthogonal :: ComplexVector -> ComplexVector -> Bool
+cvOrthogonal v1 v2 = cvInnerProd v1 v2 == complexZero
 
 -- complex matrix functions
 -- ============================================================================
@@ -201,12 +243,46 @@ printComplexMatrix cm = do putStr $ showComplexMatrix cm
 -- multiply 2 complex matrices
 -- note no size checking is performed
 cmMult :: ComplexMatrix -> ComplexMatrix -> ComplexMatrix
-cmMult cm1 cm2 = [[cvDot cv1 cv2 | cv2 <- cm2'] | cv1 <- cm1]
+cmMult cm1 cm2 = [[cvProduct cv1 cv2 | cv2 <- cm2'] | cv1 <- cm1]
     where cm2' = cmTranspose cm2
 
 -- Action of application of complex matrix A to complex vector v
 cmAction :: ComplexMatrix -> ComplexVector -> ComplexVector
-cmAction a v = [cvDot ra v | ra <- a]
+cmAction a v = [cvProduct ra v | ra <- a]
+
+-- determine inner product of 2 complex matrices
+cmInnerProd :: ComplexMatrix -> ComplexMatrix -> ComplexNumber
+cmInnerProd m1 m2 = cmTrace $ cmMult (cmDagger m1) m2
+
+-- determine trace of a complex matrix
+cmTrace :: ComplexMatrix -> ComplexNumber
+cmTrace cm = foldl complexAdd complexZero $ [r !! n | (r, n) <- zip cm [0..]]
+
+-- determine norm of a complex matrix
+cmNorm :: ComplexMatrix -> Double
+cmNorm m = sqrt $ complexRe $ cmInnerProd m m
+
+-- misc functions
+kroneckerDelta :: Int -> Int -> Int
+kroneckerDelta j k
+    | j == k    = 1
+    | otherwise = 0
+
+-- determine if a matrix is hermitian
+cmHermitian :: ComplexMatrix -> Bool
+cmHermitian m = if cmSquare m 
+                   then cmDagger m == m
+                   else error "ComplexMatrix is not square"
+
+-- determine if a complex matrix is square
+cmSquare :: ComplexMatrix -> Bool
+cmSquare m = all (== length m) $ map length m
+
+-- determine if a complex matrix is unitary
+cmUnitary :: ComplexMatrix -> Bool
+cmUnitary m = if cmSquare m
+                 then (cmMult m $ cmDagger m) == (cmIdentity $ length m)
+                 else error "ComplexMatrix is not square"
 
 -- test matrix
 cm1 :: ComplexMatrix
@@ -226,4 +302,5 @@ cm3 = [[Cartesian 1 0, Cartesian 1 1],
 cm4 :: ComplexMatrix
 cm4 = [[Cartesian 2 1, Cartesian 4 1],
        [Cartesian 3 0, Cartesian 5 0]]
+
 
